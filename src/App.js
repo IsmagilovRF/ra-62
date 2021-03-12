13c4b52efc44e4f584a4c02a6e4664c076439bb1
IsmagilovRF <ledohod@icloud.com>
2021-03-12 19:47:17 +0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import AddForm from "./components/AddForm";
+import NotesList from "./components/NotesList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notes: []
+    };
+
+    // this.dataURL = "https://ra-62.herokuapp.com/notes";
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = () => {
+    axios
+      .get(this.dataURL)
+      .then(response => this.setState({ notes: response.data }));
+  };
+
+  handleAdd = note => {
+    axios.post(this.dataURL, note).then(() => this.loadData());
+  };
+
+  handleDelete = id => {
+    axios.delete(`${this.dataURL}/${id}`).then(() => this.loadData());
+  };
+
+  render() {
+    console.log(this.state.notes);
+    return (
+      <div className="App">
+        <div className="ui text container segment">
+          <AddForm handleAdd={this.handleAdd} loadData={this.loadData} />
+          <NotesList
+            notes={this.state.notes}
+            handleDelete={this.handleDelete}
+          />
+        </div>
+        <p> learn react </p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/models/NoteModel.jsx
@@ -0,0 +1,7 @@
+export default class NoteModel {
+    constructor(id, content) {
+      this.id = id;
+      this.content = content;
+    }
+  }
+  
